d7bdced761c801198c794779322e1a53223c7ab6
JUAN FELIPE PARRA CAMARGO <jf.parrac@sis.virtual.uniandes.edu.co>
2019-03-27 12:26:21 -0500

    cliente started

---
+++ caso2/src/cliente/Cliente.java
@@ -0,0 +1,16 @@
+package cliente;
+
+public class Cliente {
+	public final static String[] CONTROL={"HOLA", "ALGORITMOS","OK","ERROR"};
+	public final static String[] SEPARADORES={";",","};
+	public final static String[] ALGS={"AES", "BLOWFISH", "RSA", "HMACSHA1", "HMACSHA256", "HMACSHA384", "HMACSHA512"};
+	public Cliente() {
+		// TODO Auto-generated constructor stub
+	}
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
