1fab286aa26c03a7356efbbec2a894d96ec1a8b5
Juan Felipe Parra <jf.parrac@uniandes.edu.co>
2019-04-06 10:59:36 -0500

    recibe y retorna strings

--- caso2/src/cliente/Simetrico.java
+++ caso2/src/cliente/Simetrico.java
@@ -7,14 +7,14 @@ import javax.crypto.SecretKey;
 public class Simetrico {
 	private final static String PADDING="AES/ECB/PKCS5Padding";
 
-	public static byte[] cifrar(SecretKey llave, String texto){
+	public static String cifrar(SecretKey llave, String texto){
 		byte[] textoCifrado;
 		try{
 			Cipher cifrador=Cipher.getInstance(PADDING);
 			byte[] textoClaro=texto.getBytes();
 			cifrador.init(Cipher.ENCRYPT_MODE, llave);
 			textoCifrado=cifrador.doFinal(textoClaro);
-			return textoCifrado;
+			return new String(textoCifrado);
 		}catch(Exception e){
 			System.out.println("Exception: "+e.getMessage());
 			return null;
@@ -22,18 +22,19 @@ public class Simetrico {
 	}
 	
 	
-	public static byte[] descifrar(SecretKey llave, byte[] txt){
+	public static String descifrar(SecretKey llave, String txt){
+		byte[] txtBytes=txt.getBytes();
 		byte[] txtClaro;
 		try{
 			Cipher cifrador=Cipher.getInstance(PADDING);
 			cifrador.init(Cipher.DECRYPT_MODE, llave);
-			txtClaro=cifrador.doFinal(txt);
+			txtClaro=cifrador.doFinal(txtBytes);
 			
 		}catch(Exception e){
 			System.out.println();
 			return null;
 		}
-		return txtClaro;
+		return new String(txtClaro);
 	}
 }
 
